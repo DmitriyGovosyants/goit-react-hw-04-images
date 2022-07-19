049e0b53dd336e7effed839f732e76e5fb414d91
DmitriyGovosyants <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-07-19 11:03:13 +0000

    Deploying to gh-pages from @ DmitriyGovosyants/goit-react-hw-04-images@cba353caf8f1ea4255d95f764fffe94cc519e233 🚀

--- src/components/App/App.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Section = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-gap: ${p => p.theme.spacing(4)};
-  padding-bottom: ${p => p.theme.spacing(6)};
-`
--- src/components/Loader/Loader.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "@emotion/styled";
-
-export const LoaderContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`
